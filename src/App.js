4645cc74dac78a9acfcdd129fc0d4fa1925e0a20
= <josephajibodu@gmail.com>
2022-03-04 01:19:24 +0100

    cart functionalities working
    Try to notify users of data persistence now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const cart = useSelector(state => state.cart)
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch('https://redux-http-40859-default-rtdb.firebaseio.com/cartItems.json', {
+        method: "PUT",
+        body: JSON.stringify(cart)
+      });
+
+      const data = await res.json();
+    }
+
+    sendRequest();
+  }, [cart])
+  
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
     </div>
   );
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../store/cart-slice";
 import "./Cart.css";
 const Cart = () => {
-  const quantity = 5;
+  const quantity = useSelector((state) => state.cart.totalQuantity);
+  const dispatch = useDispatch();
+
+  const toggleCart = () => {
+    dispatch(cartActions.showCart())
+  }
+
   return (
     <div className="cartIcon">
-      <h3>Cart: {quantity} Items</h3>
+      <h3 onClick={toggleCart}>Cart: {quantity} Items</h3>
     </div>
   );
 };
